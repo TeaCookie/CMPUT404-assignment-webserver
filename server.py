941eb0c931538871082448032513867ea0fa2ec4
TeaCookie <seaniscool22@gmail.com>
2023-09-29 03:35:15 -0600

    Modifed server.py to handle requests

--- server.py
+++ server.py
@@ -25,15 +25,89 @@ import socketserver
 # run: python freetests.py
 
 # try: curl -v -X GET http://127.0.0.1:8080/
+import os
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
 
+        if not self.data:
+            return
+        print ("Got a request of: %s\n" % self.data)
+        request = self.data.decode("utf-8").split(" ", 3)
+        method = request[0]
+        path = request[1]
+
+        if method != "GET":
+            self.request.sendall(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n<html><body><h1>405 Method Not Allowed</h1></body></html>")
+            return
+
+        error = False
+
+        if os.path.isdir(path) or os.path.isdir("./www" + path): # then this is a folder and not a file
+            if path[-1] != "/": # then redirect to the same path but with /
+                response = f"HTTP/1.1 301 Moved Permanently\r\nLocation:{path}/".encode()
+                
+
+            else: # the path is correct
+                html_file_path = "./www" + path + "index.html"
+                with open(html_file_path, "r") as f:
+                    html_content = f.read()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{html_content}".encode()
+
+                # # since the css files have different names we cannot do the same, but we can just open the first css file we find
+                # css_content = ""
+                # for filename in os.listdir(path):
+                #     if filename.endswith(".css"):
+                #         file_path = path + filename
+                #         try:
+                #             with open(file_path, "rb") as file:
+                #                 css_content = file.read()
+                #         except PermissionError as e:
+                #             response = b"HTTP/1.1 403 Forbidden\r\n\r\n<html><body><h1>403 Forbidden</h1></body></html>"
+                #             error = True
+                #         except FileNotFoundError as e:
+                #             response = b"HTTP/1.1 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>"
+                #             error = True
+                #         except Exception as e:
+                #             response = b"HTTP/1.1 500 Internal Server Error\r\n\r\n<html><body><h1>500 Internal Server Error</h1></body></html>"
+                #             error = True
+                #         break
+            
+                
+        else: # then this path is a file and just open it
+            try:
+                path = "./www" + path
+                with open(path, "r") as f:
+                    file_content = f.read()
+            except PermissionError as e:
+                response = b"HTTP/1.1 403 Forbidden\r\n\r\n<html><body><h1>403 Forbidden</h1></body></html>"
+                error = True
+            except FileNotFoundError as e:
+                response = b"HTTP/1.1 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>"
+                error = True
+            except Exception as e:
+                print(e)
+                response = b"HTTP/1.1 500 Internal Server Error\r\n\r\n<html><body><h1>500 Internal Server Error</h1></body></html>"
+                error = True
+
+
+            # deal with the mimetype
+            if (path.endswith(".css")):
+                mimetype = 'text/css'
+            elif(path.endswith(".png")):
+                mimetype = 'image/png'
+            else:
+                mimetype = 'text/html'
+
+            if not error:
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: {mimetype}\r\n\r\n{file_content}".encode("utf-8")
+        
+        self.request.sendall(response)
+        
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
